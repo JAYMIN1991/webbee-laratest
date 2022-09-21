de67e3cacba3ae1c0c3ca511a39ac85593452eda
Jaymin <jay.120421@gmail.com>
2022-09-21 16:36:06 +0530

    movie database schema

--- database/migrations/2022_06_13_075211_create_cinema_schema.php
+++ database/migrations/2022_06_13_075211_create_cinema_schema.php
@@ -37,7 +37,90 @@ class CreateCinemaSchema extends Migration
      */
     public function up()
     {
-        throw new \Exception('implement in coding task 4, you can ignore this exception if you are just running the initial migrations.');
+        //throw new \Exception('implement in coding task 4, you can ignore this exception if you are just running the initial migrations.');
+        Schema::create('movies', function (Blueprint $table) {
+            $table->increments('movieId');
+            $table->string('title')->unique();
+            $table->string('description')->nullable();
+            $table->timestamp('duration')->nullable();
+            $table->string('language');
+            $table->string('country');
+            $table->timestamps();
+        });
+
+        Schema::create('show', function (Blueprint $table) {
+            $table->id('showId')->autoIncrement()->unique();
+            $table->date('date');
+            $table->timestamp('starttime')->nullable();
+            $table->timestamp('endtime')->nullable();
+            $table->id('cinemaHallId');
+            $table->id('movieId');
+        });
+
+        Schema::create('booking', function (Blueprint $table) {
+            $table->id('bookingId')->autoIncrement();
+            $table->date('date');
+            $table->integer('numberOfSeat')->nullable();
+            $table->timestamp('datetime')->nullable();
+            $table->id('status');
+            $table->id('UserId');
+            $table->id('showId');
+            $table->timestamps();
+        });
+
+        Schema::create('User', function (Blueprint $table) {
+            $table->id('UserId')->autoIncrement();
+            $table->string('name');
+            $table->string('password')->nullable();
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->timestamps();
+        });
+
+        Schema::create('Cinema', function (Blueprint $table) {
+            $table->id('CinemaId')->autoIncrement();
+            $table->string('name');
+            $table->integer('totalCinemaHall');
+            $table->id('CityId');
+        });
+
+        Schema::create('Cinema_Hall', function (Blueprint $table) {
+            $table->id('CinemaHallId')->autoIncrement();
+            $table->string('name');
+            $table->integer('totalSeat');
+            $table->id('CinemaId');
+        });
+
+        Schema::create('Show_Seat', function (Blueprint $table) {
+            $table->id('showSeatId')->autoIncrement();
+            $table->string('status');
+            $table->integer('price');
+            $table->id('CinemaSeatId');
+            $table->id('BookingId');
+        });
+
+        Schema::create('Payment', function (Blueprint $table) {
+            $table->id('PaymentId')->autoIncrement();
+            $table->integer('amount');
+            $table->timestamp('timestamp');
+            $table->string('RemoteTransactionId');
+            $table->string('PaymentMethodtype');
+            $table->id('BookingId');
+        });
+
+        Schema::create('City', function (Blueprint $table) {
+            $table->id('CityId')->autoIncrement();
+            $table->string('name');
+            $table->string('state');
+            $table->string('zipCode');
+        });
+
+        Schema::create('Cinema_Seat', function (Blueprint $table) {
+            $table->id('CinemaSeatId')->autoIncrement();
+            $table->string('SeatNumber');
+            $table->string('type');
+            $table->id('CinemaHallId');
+        });
     }
 
     /**
@@ -47,5 +130,15 @@ class CreateCinemaSchema extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('movies');
+        Schema::dropIfExists('show');
+        Schema::dropIfExists('booking');
+        Schema::dropIfExists('User');
+        Schema::dropIfExists('Cinema');
+        Schema::dropIfExists('Cinema_Hall');
+        Schema::dropIfExists('Show_Seat');
+        Schema::dropIfExists('Payment');
+        Schema::dropIfExists('City');
+        Schema::dropIfExists('Cinema_Seat');
     }
 }
